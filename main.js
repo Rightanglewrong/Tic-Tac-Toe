b178a2e8e2d48f9ae0f346986f918459229f4f89
rightanglewrong <bennyyang996648@hotmail.com>
2022-04-05 21:20:14 -0400

    80%there, need to work on tiles

--- main.js
+++ main.js
@@ -1,57 +1,75 @@
 //Constants
-const playButtonEl = document.querySelector("#play-button")
-const resetButtonEl = document.querySelector("#reset-button")
-const possPosEls= document.querySelectorAll(".grid");
-const displayStatusEl = document.querySelector("#status")
+const playButtonEl = document.querySelector("#play-button");
+const resetButtonEl = document.querySelector("#reset-button");
+const possPosEls = document.querySelectorAll(".grid");
+const displayStatusEl = document.querySelector("#status");
 
-const players = {
-    "1": {
-        name: "Player 1",
-        turn: 0
-    },
-    "-1":{
-        name: "Player 2",
-        turn: 0
-    }
-}
+const players = [1, null, 2];
+const playerIdxX = players[0];
+const playerIdxO = players[2];
 let currentPlayer = null;
-let turn;
+let turn = 1;
 
-let board = ["","","","","","","","",""];
+let board = ["", "", "", "", "", "", "", "", ""];
 
-const winCombos = [
-    [possPosEls[0], possPosEls[1], possPosEls[2]],
-    [possPosEls[3], possPosEls[4], possPosEls[5]],
-    [possPosEls[6], possPosEls[7], possPosEls[8]],
-    [possPosEls[0], possPosEls[3], possPosEls[6]],
-    [possPosEls[1], possPosEls[4], possPosEls[7]],
-    [possPosEls[2], possPosEls[5], possPosEls[8]],
-    [possPosEls[0], possPosEls[4], possPosEls[8]],
-    [possPosEls[2], possPosEls[4], possPosEls[6]]
-      
-]
-
+// const winCombos = [
+//     [possPosEls[0], possPosEls[1], possPosEls[2]],
+//     [possPosEls[3], possPosEls[4], possPosEls[5]],
+//     [possPosEls[6], possPosEls[7], possPosEls[8]],
+//     [possPosEls[0], possPosEls[3], possPosEls[6]],
+//     [possPosEls[1], possPosEls[4], possPosEls[7]],
+//     [possPosEls[2], possPosEls[5], possPosEls[8]],
+//     [possPosEls[0], possPosEls[4], possPosEls[8]],
+//     [possPosEls[2], possPosEls[4], possPosEls[6]]
+// ]
 
 //Event Listeners
-playButtonEl.addEventListener("click",playGame)
-possPosEls.addEventListener("click", choiceMade)
-resetButtonEl.addEventListener("click", resetBoard)
-
+playButtonEl.addEventListener("click", playGame);
+resetButtonEl.addEventListener("click", resetBoard);
 
 //Functions
 
+function playGame() {
+  possPosEls.values = null;
+  currentPlayer = playerIdxX;
+  possPosEls.forEach(function (pos) {
+    pos.addEventListener("click", choiceMade);
+  });
+  displayStatusEl.textContent = `Are you ready to lose?`;
+}
 
-function playGame(e){
-    possPosEls.values = null;
-    turn = 1
-    displayStatusEl.textContent = `Are you ready to lose?`;
-};
+//Choice made on tiles
 function choiceMade(e) {
-    if (e.target)
+  let tile = e.target;
+  if (tile.value !== "" && currentPlayer === playerIdxX) {
+    tile.classList.add("x");
+    currentPlayer = playerIdxO;
+    displayStatusEl.textContent = `Turn ${turn}. It is now PLAYER${currentPlayer}'s turn!`;
+  } else if (tile.value !== "" && currentPlayer === playerIdxO) {
+    tile.classList.add("o");
+    currentPlayer = playerIdxX;
+    displayStatusEl.textContent = `Turn ${turn}. It is now PLAYER${currentPlayer}'s turn!`;
+  } else if (tile.classList.contains("x")) {
+    displayStatusEl.textContent = `Spot is taken`;
+    
+  }
+  turn = turn +1
+  if (turn === 10) {
+    turn = 1
+    // && board !== winCombos) {
+    // displayStatusEl.textContent = "ITS A DRAWWWWWWWWWWWWWW";
+  }
 }
 
 
-function resetBoard(e) {
-    playGame
-
-};
+//resets Board
+function resetBoard() {
+  possPosEls.forEach(function (tile) {
+    if (tile.classList.contains("x") || tile.classList.contains("o")) {
+      tile.classList.remove("x");
+      tile.classList.remove("o");
+    }
+  });
+  displayStatusEl.textContent = "Hold, while I erase your failures. ;)";
+  setTimeout(playGame, 3000);
+}
